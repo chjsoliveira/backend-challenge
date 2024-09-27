1db4a787b18c591a91d4215cb9c13a679b5c3f7a
chjsoliveira <chjs.oliveira@gmail.com>
2024-09-26 23:44:29 -0300

    feat: adicionar controle de status ao terraform e destroy

---
+++ infra/terraform/main.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket         = "backend-challenge-bucket-code"  # Substitua pelo nome do seu bucket
+    key            = "terraform/state"              # Caminho do arquivo de estado
+    region         = "us-east-1"                    # Substitua pela sua região
+  }
+}
