07b4f3f7f1d559144097a4516f7953670496cf2a
chjsoliveira <chjs.oliveira@gmail.com>
2024-09-28 05:18:13 -0300

    fix: tentativa nova porta

--- infra/terraform/aws-vpc.tf
+++ infra/terraform/aws-vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private_subnet_1a" {
   vpc_id            = var.main_vpc
-  cidr_block        = "172.31.1.0/24"
+  cidr_block        = "172.31.1.0/20"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -10,7 +10,7 @@ resource "aws_subnet" "private_subnet_1a" {
 
 resource "aws_subnet" "private_subnet_1b" {
   vpc_id            = var.main_vpc
-  cidr_block        = "172.31.2.0/24"
+  cidr_block        = "172.31.2.0/20"
   availability_zone = "us-east-1b"
 
   tags = {
