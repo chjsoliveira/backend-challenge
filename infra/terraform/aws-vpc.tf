0494c0663e73a38ff199013e684b62a075c31dbf
Carlos Oliveira <50434028+chjsoliveira@users.noreply.github.com>
2024-10-01 02:52:03 -0300

    fiz: ajuste nomenclatura de recursos da route table

--- infra/terraform/aws-vpc.tf
+++ infra/terraform/aws-vpc.tf
@@ -110,21 +110,16 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "private-us-east-1a" {
-  subnet_id      = aws_subnet.private-us-east-1a.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "private-us-east-1b" {
-  subnet_id      = aws_subnet.private_us_east_1b.id
+  subnet_id      = aws_subnet.private_subnet_1b.id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "public-us-east-1a" {
-  subnet_id      = aws_subnet.public_us_east_1a.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public-us-east-1b" {
-  subnet_id      = aws_subnet.public_us_east_1b.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
   route_table_id = aws_route_table.public.id
 }
