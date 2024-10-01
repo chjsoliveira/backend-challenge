20c698b0601049631db82c3b3fbafea24187af77
Carlos Oliveira <50434028+chjsoliveira@users.noreply.github.com>
2024-10-01 00:57:23 -0300

    feat: criar NAT via terraform, devido a erro ao adicionar instância en2 no kubernets

--- infra/terraform/aws-vpc.tf
+++ infra/terraform/aws-vpc.tf
@@ -1,36 +1,73 @@
+# Subnet pública 1 para o NAT Gateway
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id            = var.main_vpc
+  cidr_block        = "172.31.64.0/20"  
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+# Subnet privada 1a
 resource "aws_subnet" "private_subnet_1a" {
   vpc_id            = var.main_vpc
-  cidr_block        = "172.31.96.0/20" 
+  cidr_block        = "172.31.96.0/20"
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "private-subnet"
+    Name = "private-subnet-1"
   }
 }
 
+# Subnet privada 1b
 resource "aws_subnet" "private_subnet_1b" {
   vpc_id            = var.main_vpc
-  cidr_block        = "172.31.112.0/20" 
+  cidr_block        = "172.31.112.0/20"
   availability_zone = "us-east-1b"
 
   tags = {
-    Name = "private-subnet"
+    Name = "private-subnet-2"
   }
 }
 
-# (Opcional) Criação de uma rota para a sub-rede privada (se necessário)
+# EIP para o NAT Gateway
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+# NAT Gateway na subnet pública
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1a.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+}
+
+# Tabela de rotas para a sub-rede privada
 resource "aws_route_table" "private_rt" {
   vpc_id = var.main_vpc
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id  # Rota para o NAT Gateway
+  }
 
   tags = {
     Name = "private-route-table"
   }
 }
 
+# Associa a tabela de rotas à subnet privada 1a
 resource "aws_route_table_association" "private_subnet_1a_association" {
   subnet_id      = aws_subnet.private_subnet_1a.id
   route_table_id = aws_route_table.private_rt.id
 }
+
+# Associa a tabela de rotas à subnet privada 1b
 resource "aws_route_table_association" "private_subnet_1b_association" {
   subnet_id      = aws_subnet.private_subnet_1b.id
   route_table_id = aws_route_table.private_rt.id
