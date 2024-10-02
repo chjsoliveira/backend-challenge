4d5858db16d89df87afab2c32b46b5a90b06729e
Carlos Oliveira <50434028+chjsoliveira@users.noreply.github.com>
2024-10-02 15:03:39 -0300

    feat: revisão do security group

--- infra/terraform/aws-sg.tf
+++ infra/terraform/aws-sg.tf
@@ -1,34 +1,28 @@
 resource "aws_security_group" "lb_security_group" {
-  name        = "lb-security-group"
-  description = "Allow traffic for Load Balancer"
-  vpc_id      = var.main_vpc
+  name   = "lb-security-group"
+  vpc_id = var.vpc_id
 
+  # Ingress: Permitir tráfego HTTP/HTTPS de 0.0.0.0/0
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Aberto ao público
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Aberto ao público
   }
-  
-  ingress {
-    from_port   = 30001
-    to_port     = 30001
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
+
+  # Egress: Permitir tráfego para os nós do cluster na porta 30001
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port                = 30001
+    to_port                  = 30001
+    protocol                 = "tcp"
+    destination_security_group_id = aws_security_group.els_security_group.id  # Security Group dos nós do cluster
   }
 
   tags = {
@@ -38,35 +32,22 @@ resource "aws_security_group" "lb_security_group" {
 
 
 resource "aws_security_group" "eks_security_group" {
-  name        = "eks-security-group"
-  description = "Allow traffic for EKS nodes"
-  vpc_id      = var.main_vpc
+  name   = "eks-security-group"
+  vpc_id = var.vpc_id
 
+  # Ingress: Permitir tráfego na porta 30001 vindo do Load Balancer
   ingress {
-    from_port   = 10250
-    to_port     = 10250
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]  # Ajuste de acordo com sua CIDR de VPC
+    from_port                = 30001
+    to_port                  = 30001
+    protocol                 = "tcp"
+    source_security_group_id = aws_security_group.lb_security_group.id  # Security Group do Load Balancer
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    security_groups = [aws_security_group.lb_security_group.id]  # Permitir tráfego do Load Balancer
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    security_groups = [aws_security_group.lb_security_group.id]  # Permitir tráfego do Load Balancer
-  }
-
+  # Egress: Permitir todo o tráfego de saída
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1"         # All traffic
     cidr_blocks = ["0.0.0.0/0"]
   }
 
